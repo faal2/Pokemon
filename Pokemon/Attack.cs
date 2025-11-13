28a16c2feba447cde07eedcb3aeb18dd9ff9e169
faal <130977909+faal2@users.noreply.github.com>
2025-11-13 09:39:32 +0100

    attack klassen

---
+++ Pokemon/Attack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon
+{
+    internal class Attack
+    {
+        private string _name;
+        private ElementType _type;
+        private int _basePower;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+
+        public ElementType Type
+        {
+            get
+            {
+                return _type;
+            }
+            set
+            {
+                _type = value;
+            }
+        }
+
+        public int BasePower
+        {
+            get
+            {
+                return _basePower;
+            }
+            set
+            {
+                _basePower = value;
+            }
+
+        }
+
+        public Attack(string name, ElementType type, int basePower)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.BasePower = basePower;
+        }
+
+        public void Use (int level)
+        {
+            Console.WriteLine($"{this.Name} hit with a total power of {level + this.BasePower}");
+        }
+    }
+}
