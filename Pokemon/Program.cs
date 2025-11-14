b8cff00f9626612bc81f8ca06e2818b6e25e1e77
faal <130977909+faal2@users.noreply.github.com>
2025-11-14 14:39:22 +0100

    added first draft of the pokemon program

--- Pokemon/Program.cs
+++ Pokemon/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Pokemon
+﻿using System;
+
+namespace Pokemon
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Attack waterthrower = new Attack("Waterthrower", ElementType.Water, 12);
+            Attack waterbomb = new Attack("Waterbomb", ElementType.Water, 6);
+            Attack watersword = new Attack("Watersword", ElementType.Water, 3);
+
+            List<Pokemon> pokemons = new ()
+            {
+            new Squirtle([waterthrower, waterbomb]),
+            };
+
+            foreach (var pokemon in pokemons)
+            {
+                Console.WriteLine($"Använder {pokemon.Name} som är typ {pokemon.Type} och level {pokemon.Level}.");
+
+                pokemon.Attack();
+
+                pokemon.RaiseLevel();
+
+                if (pokemon is IEvolvable evolvablePokemon)
+                {
+                    evolvablePokemon.Evolve();
+                }
+
+                Console.WriteLine("\n");
+            }
+
+
         }
     }
 }
