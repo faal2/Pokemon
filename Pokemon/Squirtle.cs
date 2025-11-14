057ead0e9049bcf9f4029c9d1d2e5d9dfbbfe7f3
faal <130977909+faal2@users.noreply.github.com>
2025-11-14 14:38:19 +0100

    added squirtle constructur and evolve (required method)

---
+++ Pokemon/Squirtle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon
+{
+    internal class Squirtle : WaterPokemon, IEvolvable
+    {
+
+
+        public Squirtle(List<Attack> attacks) : base("Squirtle", attacks)
+        {
+        }
+
+        public void Evolve()
+        {
+
+        }
+}
+}
